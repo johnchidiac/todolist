863cef7cfb4c6b85bab754219351d9910bbb54a8
John Chidiac <johnchidiac@seneca.local>
2023-08-15 12:45:37 -0400

    Completing assignment but with completing and flagging tasks undone

--- app.js
+++ app.js
@@ -1,72 +1,141 @@
+const taskForm = document.querySelector('#taskForm');
+const taskList = document.querySelector('#taskList');
+const newTask = document.querySelector('input[name="taskName"]');
 
-// const newItemForm = document.querySelector('#addItem');
-// const taskList = document.querySelector('#actionItems');
-// const removeButtons = document.querySelectorAll('li button');
-// const actionItem = document.querySelector('input[name="actionItemName"]');
-// for (let btn of removeButtons) {
-// 	btn.addEventListener('click', function(e) {
-// 		e.target.parentElement.remove();
-// 	})
-// }
+function getStoredTasks() {
+	const storedTasksJSON = localStorage.getItem('Tasks');
+	return (storedTasksJSON) ? JSON.parse(storedTasksJSON) : {};
+}
 
-// newItemForm.addEventListener('submit', function(e, ) {
-// 	e.preventDefault();
-// 	const inboxItem = addItemToInbox(actionItem.value);
-// });
+function setStoredTasks(tasks) {
+	localStorage.setItem('Tasks', JSON.stringify(tasks));
+}
 
-// function addItemToInbox(name) {
-// 	const newActionItem = document.createElement('li');
-// 	const actionItemDelete = document.createElement('button');
-// 	actionItemDelete.innerText = 'x';
+taskList.addEventListener('click', function(e) {
+
+	const clickedItem = e.target;
 	
-// 	newActionItem.innerText = name;
-// 	newActionItem.value = '';
-// 	const newTask = taskList.appendChild(newActionItem);
-// 	newTask.appendChild(actionItemDelete);
-	
-	
-// }
-
-
-const newItemForm = document.querySelector('#addItem');
-const taskList = document.querySelector('#actionItems');
-const actionItem = document.querySelector('input[name="actionItemName"]');
-
-taskList.addEventListener('click', function(event) {
-	if (event.target.tagName === 'BUTTON') {
-		event.target.parentElement.remove();
+	if (clickedItem.tagName === 'BUTTON') { // DELETE
+		const taskName = clickedItem.parentElement.querySelector('.taskName').innerText;
+		deleteTask(taskName);
+		// clickedItem.parentElement.remove();
 	}
-	if (event.target.tagName === 'SPAN') {
-		event.target.parentElement.classList.toggle('flagged');
-	}
-	if (event.target.tagName === 'INPUT') {
-		event.target.parentElement.classList.toggle('complete');
-	}
-})
 
+	if (clickedItem.tagName === 'SPAN') { // FLAG
+		const taskName = clickedItem.parentElement.querySelector('.taskName').innerText;
+		const isFlagged = clickedItem.parentElement.classList.contains('flagged');
+		if (isFlagged) {
+			// clickedItem.parentElement.classList.remove('flagged');
+			updateTask(taskName, 'isFlagged', 'false');
+		} else {
+			// clickedItem.parentElement.classList.add('flagged');
+			updateTask(taskName, 'isFlagged', 'true');
+		}
+	}
 
-newItemForm.addEventListener('submit', function(e) {
-	e.preventDefault();
-	const inboxItem = addItemToInbox(actionItem.value);
+	if (clickedItem.tagName === 'INPUT') { // CHECKBOX
+		
+		if (clickedItem.checked) {
+			updateTask(clickedItem.parentElement.querySelector('.taskName').innerText, 'isComplete', 'false');
+			// clickedItem.parentElement.classList.add('complete');
+		} else {
+			updateTask(clickedItem.parentElement.querySelector('.taskName').innerText, 'isComplete', 'true');
+			// clickedItem.parentElement.classList.remove('complete');
+		}
+	}
+
 });
 
-function addItemToInbox(name) {
-	const newActionItem = document.createElement('li');
-	const actionItemName = document.createElement('span');
-	const actionItemDelete = document.createElement('button');
-	const flag = document.createElement('span');
-	flag.innerHTML = '&#9873;'
-	flag.classList.add('flag');
-	actionItemDelete.innerText = 'x';
-	actionItemName.innerText = name;
-	actionItem.value = '';
-	const newTask = taskList.appendChild(newActionItem);
-	const actionItemNameText = newTask.appendChild(actionItemName);
-	actionItemNameText.classList.add('task');
-	newTask.appendChild(actionItemDelete);
-	
-	newTask.prepend(flag);
-	const checkbox = document.createElement('input');
-	checkbox.type = 'checkbox';
-	newTask.prepend(checkbox);
+
+function populateTaskList() {
+    
+	const storedTasks = getStoredTasks();
+        
+    // Clear the taskList before repopulating it
+    taskList.textContent = '';
+    
+    if (storedTasks) {
+        
+        for (let task in storedTasks) {
+            const taskListItem = document.createElement('li');
+    
+            const taskName = document.createElement('span');
+            taskName.innerText = task;
+            taskName.classList.add('taskName');
+            taskListItem.appendChild(taskName);
+            
+            const taskFlag = document.createElement('span');
+            taskFlag.innerHTML = '&#9873;';
+            taskFlag.classList.add('flag');
+            const isFlagged = (storedTasks[task].isFlagged === "true") ? 'flagged' : '';
+            if (isFlagged) { taskFlag.classList.add(isFlagged); }
+            taskListItem.prepend(taskFlag);
+    
+            const taskDeleteButton = document.createElement('button');
+            taskDeleteButton.innerHTML = '&#x2715;';
+			taskDeleteButton.classList.add('hidden');
+            taskListItem.appendChild(taskDeleteButton);
+    
+            const taskStatusCheckbox = document.createElement('input');
+            const completeStatus = (storedTasks[task].isComplete === "true") ? true : false;
+            taskStatusCheckbox.type = 'checkbox';
+            taskStatusCheckbox.checked = completeStatus;
+            taskListItem.prepend(taskStatusCheckbox);
+        
+            taskList.appendChild(taskListItem);
+        }
+        
+    } else {
+        const emptyState = document.createElement('li');
+        emptyState.innerText = "No tasks remaining to do!";
+        emptyState.id = 'emptyList';
+        taskList.appendChild(emptyState); // Changed from replaceChildren to appendChild
+    }
 }
+
+populateTaskList();
+
+function addTask(name) {
+	const storedTasks = getStoredTasks();
+
+	storedTasks[name] = {
+		"isComplete": "false",
+		"isFlagged": "false"
+	};
+	setStoredTasks(storedTasks);
+
+	populateTaskList();
+}
+
+
+function updateTask(name, attribute, value) {
+	// Get the storedTasks object from localStorage and parse it
+	const storedTasks = getStoredTasks();
+  
+	// Update the task attribute in the storedTasks object
+	if (storedTasks[name]) {
+	  storedTasks[name][attribute] = value;
+  
+	  setStoredTasks(storedTasks);
+	  
+	  populateTaskList();
+
+	} else {
+	  console.error(`Task "${name}" not found!`);
+	}
+  }
+  
+  function deleteTask(name) {
+	const storedTasks = getStoredTasks();
+
+	delete storedTasks[name];
+	setStoredTasks(storedTasks);
+
+	populateTaskList();
+}
+
+taskForm.addEventListener('submit', function(e) {
+	e.preventDefault();
+	addTask(newTask.value);
+	newTask.value = ''; // Clear field after submission
+});
